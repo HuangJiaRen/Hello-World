204b135d320251f657c5157b372ed7c235329e94
huangli323 <huangli323@xdf.cn>
2019-06-13 22:37:02 +0800

    调整结构

---
+++ mix-study/src/main/java/com/xdf/huangli/interrupt/InterruptThread.java
@@ -0,0 +1,12 @@
+package com.xdf.huangli.interrupt;
+
+public class InterruptThread{
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(){
+			public void run(){
+
+			}
+		};
+	}
+}
