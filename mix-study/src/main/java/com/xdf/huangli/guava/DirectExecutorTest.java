97f8fc596767583dc7b672d33c715a061b88f4c8
huangli <1466384630@qq.com>
2019-07-28 22:42:13 +0800

    提交线程池学习demo:guava+JMH

---
+++ mix-study/src/main/java/com/xdf/huangli/guava/DirectExecutorTest.java
@@ -0,0 +1,21 @@
+package com.xdf.huangli.guava;
+
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author huangli
+ * @version 1.0
+ * @description TODO
+ * @date 2019-07-27 22:08
+ */
+public class DirectExecutorTest {
+    public static void main(String[] args) {
+        Executor executor = MoreExecutors.directExecutor();
+        executor.execute(() -> System.out.println("I am running in " +
+                Thread.currentThread().getName()));
+    }
+
+
+}
