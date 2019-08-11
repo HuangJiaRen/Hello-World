1de6d0841b4a48b7ac76901e0b89a8b9cd74720c
huangli <1466384630@qq.com>
2019-08-11 21:04:19 +0800

    学习Future

---
+++ mix-study/src/main/java/com/xdf/huangli/future/Data.java
@@ -0,0 +1,11 @@
+package com.xdf.huangli.future;
+
+/**
+ * @author huangli
+ * @version 1.0
+ * @description TODO
+ * @date 2019-08-11 09:56
+ */
+public interface Data {
+    public String getResult();
+}
