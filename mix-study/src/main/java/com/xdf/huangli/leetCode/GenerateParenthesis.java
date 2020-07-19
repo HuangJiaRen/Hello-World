c30a814fd5dae3b4ae052c17c2634cc65af3ab13
huangli <1466384630@qq.com>
2020-07-19 16:54:50 +0800

    数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。

---
+++ mix-study/src/main/java/com/xdf/huangli/leetCode/GenerateParenthesis.java
@@ -0,0 +1,76 @@
+package com.xdf.huangli.leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author huangli
+ * @version 1.0
+ * @description TODO
+ * @date 2020-07-19 15:09
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ * 示例：
+ *
+ * 输入：n = 3
+ * 输出：[
+ *        "((()))",
+ *        "(()())",
+ *        "(())()",
+ *        "()(())",
+ *        "()()()"
+ *      ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GenerateParenthesis {
+
+    public static void main(String[] args) {
+        List<String> strings = generateParenthesis(3);
+        System.out.println(strings.toString());
+    }
+
+    // 做减法
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        // 特判
+        if (n == 0) {
+            return res;
+        }
+
+        // 执行深度优先遍历，搜索可能的结果
+        dfs("", n, n, res);
+        return res;
+    }
+
+    /**
+     * @param curStr 当前递归得到的结果
+     * @param left   左括号还有几个可以使用
+     * @param right  右括号还有几个可以使用
+     * @param res    结果集
+     */
+    private static void dfs(String curStr, int left, int right, List<String> res) {
+        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+        // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+
+        // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+        if (left > right) {
+            return;
+        }
+
+        if (left > 0) {
+            dfs(curStr + "(", left - 1, right, res);
+        }
+
+        if (right > 0) {
+            dfs(curStr + ")", left, right - 1, res);
+        }
+    }
+}
