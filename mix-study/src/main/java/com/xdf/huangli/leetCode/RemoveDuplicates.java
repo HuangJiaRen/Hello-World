6d3e4ae6511b7acc6e075dea9095afbb2e311df1
huangli <1466384630@qq.com>
2020-08-10 21:19:35 +0800

    update:leetcode

--- mix-study/src/main/java/com/xdf/huangli/leetCode/RemoveDuplicates.java
+++ mix-study/src/main/java/com/xdf/huangli/leetCode/RemoveDuplicates.java
@@ -27,12 +27,12 @@ package com.xdf.huangli.leetCode;
  * 方法：双指针法
  * 算法
  *
- * 数组完成排序后，我们可以放置两个指针 ii 和 jj，其中 ii 是慢指针，
- * 而 jj 是快指针。只要 nums[i] = nums[j]nums[i]=nums[j]，我们就增加 jj 以跳过重复项。
+ * 数组完成排序后，我们可以放置两个指针 i 和 j，其中 i 是慢指针，
+ * 而 j 是快指针。只要 nums[i] = nums[j]，我们就增加 j 以跳过重复项。
  *
  * 当我们遇到 nums[j] != nums[i]，
  * 跳过重复项的运行已经结束，因此我们必须把它（nums[j]）的值复制到
- * nums[i + 1]。然后递增 ii，接着我们将再次重复相同的过程，直到 jj 到达数组的末尾为止。
+ * nums[i + 1]。然后递增 i，接着我们将再次重复相同的过程，直到 j 到达数组的末尾为止。
  *
  */
 public class RemoveDuplicates {
@@ -51,13 +51,16 @@ public class RemoveDuplicates {
             }
 
             int i = 0;
+
             for (int j = 1; j < nums.length; j++) {
-                if (nums[j] != nums[i]) {
-                    i++;
+                if(nums[j] != nums[i]){
+                    i ++;
                     nums[i] = nums[j];
                 }
             }
-            return i + 1;
+
+
+           return i+1;
     }
 
 }
